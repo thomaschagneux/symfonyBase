d4efc7600457d43a6d078a3e1e31d818bf828460
Thomas Chagneux <thomas.chagneux@gmail.com>
2023-12-01 00:09:18 +0100

    remove datatable bundle

--- config/bundles.php
+++ config/bundles.php
@@ -13,5 +13,4 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    Omines\DataTablesBundle\DataTablesBundle::class => ['all' => true],
 ];
